d32d335fe978dee9cc503db28c15d4d1b5c055ee
Miroslav Pakanec <mpakan@cs.aau.dk>
2021-01-22 09:31:48 +0100

    added params to test script

--- run_test.py
+++ run_test.py
@@ -3,4 +3,13 @@ import editdistance
 import tensorflow as tf
 
 print('loaded libs.')
+
+if __name__ == "__main__":
+    config_file = sys.argv[1]
+    if len(sys.argv) > 1:
+        run_name = sys.argv[2]
+    else:
+        run_name = datetime.datetime.now().strftime('%m%d%Y-%H%M%S')
+    print(f'config:{config_file}')
+    print(f'run name: {run_name}')
 print('done.)
